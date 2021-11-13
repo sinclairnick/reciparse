09cb6c8959cab64d5297b13158b3dcabef3f3153
Nick Sinclair <nick@parkable.com>
2021-11-14 11:27:58 +1300

    Return array of hrecipes

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,15 +2,17 @@ import { parseFromUrl } from "."
 
 const urls = [
 	["Epicurious", "https://www.epicurious.com/recipes/food/views/hasselback-butternut-squash-with-bay-leaves"],
-	["AllRcipes", "https://www.allrecipes.com/recipe/230103/buttery-garlic-green-beans/"]
+	["AllRcipes", "https://www.allrecipes.com/recipe/230103/buttery-garlic-green-beans/"],
+	["BBC", "https://www.bbcgoodfood.com/recipes/chilli-con-carne-recipe"]
 ]
 
 describe("Tests", () => {
 	it.each(urls)("Should parse a websites content and extract hrecipe data (%s)", async (name, url) => {
 		const res = await parseFromUrl(url)
 
-		expect(res?.["@type"]).toBe("Recipe")
-		expect(res?.recipeIngredient).toBeDefined()
-		expect(res?.recipeIngredient?.length).toBeGreaterThan(0)
+		const [first] = res
+		expect(first?.["@type"]).toBe("Recipe")
+		expect(first?.recipeIngredient).toBeDefined()
+		expect(first?.recipeIngredient?.length).toBeGreaterThan(0)
 	})
 })
--- src/index.ts
+++ src/index.ts
@@ -3,29 +3,35 @@ import cheerio from "cheerio"
 import { HRecipe } from "./types"
 
 const isHRecipe = (val: any): val is HRecipe => "@type" in val && val["@type"] === "Recipe"
+const isNotNull = <T = any>(val?: T | null): val is T => val != null
 
 export const parseFromUrl = async (url: string) => {
 	try {
 		const res = await axios.get(url)
 		const $ = cheerio.load(res.data)
 		const appScripts = $("script[type*=application]")
-		const html = appScripts.html()
+		const htmls = appScripts
+			.toArray()
+			.map((el) => $(el).html())
+			.filter(isNotNull);
 
-		if (html == null || html.length === 0) {
-			return
+		const candidates: HRecipe[] = []
+
+		for (const html of htmls) {
+			const data = JSON.parse(html)
+			if (Array.isArray(data)) {
+				const hrecipe = data.find(isHRecipe)
+				if (hrecipe != null) {
+					candidates.push(hrecipe)
+				}
+				continue
+			}
+			if (isHRecipe(data)) {
+				candidates.push(data)
+			}
 		}
 
-		const data = JSON.parse(html)
-
-		if (Array.isArray(data)) {
-			return data.find(isHRecipe)
-		}
-
-		if (isHRecipe(data)) {
-			return data
-		}
-
-		return
+		return candidates
 	} catch (e) {
 		throw e
 	}
