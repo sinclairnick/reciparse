98fd8baa82d70d5940502053b96321d6a1785870
Nick Sinclair <nick@parkable.com>
2022-05-06 16:22:58 +1200

    Improve structured data parsing

--- src/parsing/structured/structured-data.test.ts
+++ src/parsing/structured/structured-data.test.ts
@@ -1,40 +1,64 @@
-import { extractFromStructuredData } from "./structured-data"
-import axios from "axios"
+import { extractFromStructuredData } from "./structured-data";
+import axios from "axios";
 
 const structuredRecipes = [
-	["Epicurious", "https://www.epicurious.com/recipes/food/views/hasselback-butternut-squash-with-bay-leaves"],
-	["AllRcipes", "https://www.allrecipes.com/recipe/230103/buttery-garlic-green-beans/"],
-	["BBC", "https://www.bbcgoodfood.com/recipes/chilli-con-carne-recipe"],
-	["BBC 2", "https://www.bbcgoodfood.com/recipes/chicken-kale-mushroom-pot-pie"],
-	["BBC 3", "https://www.bbcgoodfood.com/recipes/3-veg-mac-n-cheese"],
-	["Cafe delites", "https://cafedelites.com/butter-chicken/"],
-	["Natashas kitchen", "https://natashaskitchen.com/perfect-burger-recipe/"],
-	["Jamie Oliver", "https://www.jamieoliver.com/recipes/chicken-recipes/chicken-tikka-masala/"]
-]
+  [
+    "Epicurious",
+    "https://www.epicurious.com/recipes/food/views/hasselback-butternut-squash-with-bay-leaves",
+  ],
+  [
+    "AllRcipes",
+    "https://www.allrecipes.com/recipe/230103/buttery-garlic-green-beans/",
+  ],
+  ["BBC", "https://www.bbcgoodfood.com/recipes/chilli-con-carne-recipe"],
+  [
+    "BBC 2",
+    "https://www.bbcgoodfood.com/recipes/chicken-kale-mushroom-pot-pie",
+  ],
+  ["BBC 3", "https://www.bbcgoodfood.com/recipes/3-veg-mac-n-cheese"],
+  ["Cafe delites", "https://cafedelites.com/butter-chicken/"],
+  ["Natashas kitchen", "https://natashaskitchen.com/perfect-burger-recipe/"],
+  [
+    "Jamie Oliver",
+    "https://www.jamieoliver.com/recipes/chicken-recipes/chicken-tikka-masala/",
+  ],
+  [
+    "Delish",
+    "https://www.delish.com/cooking/recipe-ideas/a28848480/beef-ragu-recipe/",
+  ],
+  [
+    "Spruce eats",
+    "https://www.thespruceeats.com/braised-pork-belly-recipe-1135629",
+  ],
+];
 
 /** Just using these to smoke test, they are imperfectly represented in structured form on their side */
 // const problematicRecipes: [string, string][] = []
 
 describe("Structured data", () => {
+  it.each(structuredRecipes)(
+    "Parses and transforms recipes with structured data (%s)",
+    async (name, url) => {
+      const res = await axios.get(url);
+      const html = res.data;
+      const [first] = extractFromStructuredData(html);
 
-	it.each(structuredRecipes)("Parses and transforms recipes with structured data (%s)", async (name, url) => {
-		const res = await axios.get(url)
-		const html = res.data
-		const [first] = extractFromStructuredData(html)
+      expect(first.title).toBeDefined();
+      expect(first.ingredients.length).toBeGreaterThan(0);
+      expect(first.ingredients.some((x) => /\<.*\>/.test(x.name ?? ""))).toBe(
+        false
+      );
+      expect(first.steps.length).toBeGreaterThan(0);
+      // Contains html
+      expect(first.yield).toBeDefined();
+    }
+  );
 
-		expect(first.title).toBeDefined()
-		expect(first.steps.length).toBeGreaterThan(0)
-		expect(first.ingredients.length).toBeGreaterThan(0)
-		expect(first.yield).toBeDefined()
-	})
+  // it.each(problematicRecipes)("Does not error for problematic recipes (%s)", async (name, url) => {
+  // 	const res = await axios.get(url)
+  // 	const html = res.data
+  // 	const [first] = extractFromStructuredData(html)
 
-	// it.each(problematicRecipes)("Does not error for problematic recipes (%s)", async (name, url) => {
-	// 	const res = await axios.get(url)
-	// 	const html = res.data
-	// 	const [first] = extractFromStructuredData(html)
-
-	// 	console.log(first)
-	// })
-
-
-})
+  // 	console.log(first)
+  // })
+});
--- src/parsing/structured/structured-data.ts
+++ src/parsing/structured/structured-data.ts
@@ -1,24 +1,46 @@
-import * as HTMLParser from "node-html-parser"
+import * as HTMLParser from "node-html-parser";
 import { isNotNull } from "../../common/constants";
-import { extractIngredientInfo, extractYieldInfo } from "../../common/extractors";
+import {
+  extractIngredientInfo,
+  extractYieldInfo,
+} from "../../common/extractors";
 import { Recipe } from "../../common/types";
 import { SchemaOrgRecipe } from "./structured.types";
 
-const isStructuredRecipe = (val: any): val is SchemaOrgRecipe => "@type" in val && val["@type"] === "Recipe"
+const isStructuredRecipe = (val: any): val is SchemaOrgRecipe => {
+  const hasAtType = "@type" in val;
+  if (!hasAtType) return false;
+  const typeValue = val["@type"];
 
-const extractAuthors = (authors: SchemaOrgRecipe["author"]): Recipe["authors"] => {
-	if (authors == null) {
-		return []
-	}
-	if (Array.isArray(authors)) {
-		return authors.map(a => ({ name: a.name }))
-	}
-	return [{ name: authors.name }]
-}
+  if (typeof typeValue === "string") {
+    return typeValue.toLowerCase() === "recipe";
+  }
 
-const extractIngredients = (ingredients: SchemaOrgRecipe["recipeIngredient"]): Recipe["ingredients"] => {
-	return Array.isArray(ingredients) ? ingredients?.map(ing => extractIngredientInfo(ing)) : []
-}
+  if (Array.isArray(typeValue)) {
+    return typeValue.includes("Recipe");
+  }
+  return false;
+};
+
+const extractAuthors = (
+  authors: SchemaOrgRecipe["author"]
+): Recipe["authors"] => {
+  if (authors == null) {
+    return [];
+  }
+  if (Array.isArray(authors)) {
+    return authors.map((a) => ({ name: a.name }));
+  }
+  return [{ name: authors.name }];
+};
+
+const extractIngredients = (
+  ingredients: SchemaOrgRecipe["recipeIngredient"]
+): Recipe["ingredients"] => {
+  return Array.isArray(ingredients)
+    ? ingredients?.map((ing) => extractIngredientInfo(ing))
+    : [];
+};
 
 const extractSteps = (
   steps: SchemaOrgRecipe["recipeInstructions"]
@@ -28,60 +50,59 @@ const extractSteps = (
   }
   if (typeof steps === "string") {
     const html = HTMLParser.parse(steps);
-	const parent = html.childNodes.length > 1 ? html : html.childNodes[0]
+    const parent = html.childNodes.length > 1 ? html : html.childNodes[0];
     return parent?.childNodes.map((x) => ({ text: x.innerText }));
   }
   return [];
 };
 
 const transformSchemaOrgRecipe = (data: SchemaOrgRecipe): Recipe => {
-	const authors = extractAuthors(data.author)
-	const ingredients = extractIngredients(data.recipeIngredient)
-	const steps = extractSteps(data.recipeInstructions)
-	const yieldResult = extractYieldInfo(data.recipeYield)
+  const authors = extractAuthors(data.author);
+  const ingredients = extractIngredients(data.recipeIngredient);
+  const steps = extractSteps(data.recipeInstructions);
+  const yieldResult = extractYieldInfo(data.recipeYield);
 
-	return {
-		title: data.name,
-		steps,
-		authors,
-		ingredients,
-		yield: yieldResult.amount,
-		yieldMetric: yieldResult.metric,
-	}
-}
+  return {
+    title: data.name,
+    steps,
+    authors,
+    ingredients,
+    yield: yieldResult.amount,
+    yieldMetric: yieldResult.metric,
+  };
+};
 
 export const extractFromStructuredData = (html: string): Recipe[] => {
-	const document = HTMLParser.parse(html)
-	const appScripts = document.querySelectorAll("script[type=application/ld+json]")
-	const htmls = appScripts
-		.map((el) => el.innerHTML)
-		.filter(isNotNull);
+  const document = HTMLParser.parse(html);
+  const appScripts = document.querySelectorAll(
+    "script[type='application/ld+json']"
+  );
+  const htmls = appScripts.map((el) => el.innerHTML).filter(isNotNull);
 
+  const candidates: SchemaOrgRecipe[] = [];
 
-	const candidates: SchemaOrgRecipe[] = []
+  for (const html of htmls) {
+    const data = JSON.parse(html);
+    if (Array.isArray(data)) {
+      const hrecipe = data.find(isStructuredRecipe);
+      if (hrecipe != null) {
+        candidates.push(hrecipe);
+      }
+      continue;
+    }
+    if ("@graph" in data) {
+      const maybeRecipes = data["@graph"];
+      for (const maybeRecipe of maybeRecipes) {
+        if (isStructuredRecipe(maybeRecipe)) {
+          candidates.push(maybeRecipe);
+        }
+      }
+    }
+    if (isStructuredRecipe(data)) {
+      candidates.push(data);
+    }
+  }
 
-	for (const html of htmls) {
-		const data = JSON.parse(html)
-		if (Array.isArray(data)) {
-			const hrecipe = data.find(isStructuredRecipe)
-			if (hrecipe != null) {
-				candidates.push(hrecipe)
-			}
-			continue
-		}
-		if ("@graph" in data) {
-			const maybeRecipes = data["@graph"]
-			for (const maybeRecipe of maybeRecipes) {
-				if (isStructuredRecipe(maybeRecipe)) {
-					candidates.push(maybeRecipe)
-				}
-			}
-		}
-		if (isStructuredRecipe(data)) {
-			candidates.push(data)
-		}
-	}
-
-	const recipes: Recipe[] = candidates.map(transformSchemaOrgRecipe)
-	return recipes
-}
+  const recipes: Recipe[] = candidates.map(transformSchemaOrgRecipe);
+  return recipes;
+};
